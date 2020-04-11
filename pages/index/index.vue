1b5abb26333776deaa086bf458636bd70ead4f0e
CtrlCVTian <782218476@qq.com>
2020-04-11 09:57:24 +0800

    项目初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Description: 
+ * @Author: tiange
+ * @Date: 2020-04-11 09:35:59
+ * @LastEditTime: 2020-04-11 09:52:46
+ * @LastEditors: tiange
+ -->
+<template>
+  <view class="content">
+    <view class="content-top"></view>
+    <view class="content-bottom"></view>
+  </view>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Hello'
+    }
+  },
+  onLoad () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
